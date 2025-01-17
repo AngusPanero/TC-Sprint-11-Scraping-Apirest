c2e2644a23d2edbb7bd2b2d78ede7e625ca01743
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2025-01-17 17:52:47 -0300

    EMPEZANDO CRUD

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
+const noticiasDia = require("./scraping");
 const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio")
 const fs = require("fs");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const noticiasDos = [];
+
+const clonarNoticias = async () => {
+    const noticiasClon = await noticiasDia();
+    noticiasDos.push(...noticiasClon)
+}
+clonarNoticias();
+
 app.get("/", (req, res) => {
-    res.send("El Peluca Sape!!!")
+    res.send(`
+        <h1>Noticias del Día - El País</h1>
+        <a href="/noticias">
+        <p>Mira Las Últimas Noticias del Día</p>
+        </a>`)
 })
 
-app.listen(8000, () => {
-    console.log("Server Linstening On PORT, http://localhost:8000");
+//READ
+app.get("/noticias", (req, res) => {
+    try {
+        res.send(`
+        ${noticiasDos.map((noticia) => `
+            <h1>${noticia.titulo}</h1>
+            <img src="${noticia.imagen}" alt="Imagen Obtenida del Periódico El País" style="max-width:200px;">
+            <p>${noticia.descripcion}</p>
+            <a href="${noticia.enlace}"><p>Noticia Completa</p></a>
+            `).join("")}
+        `);
+    } 
+    catch (error) {
+        res.status(500).send("Error en la Respuesta");
+    }
+});
+
+app.post("/noticias", (req, res ) => {
+    const nuevaNoticia = {
+        titulo: req.body.titulo,
+        imagen: req.body.imagen,
+        descripcion: req.body.descripcion,
+        enlace: req.body.enlace,
+    }
+})
+
+app.use((req, res) => {
+    res.send(`<h1>404 - No se ha Encontrado la Página</h1>`)
+})
+
+app.listen(9000, () => {
+    console.log("Server Linstening On PORT, http://localhost:9000");
 })
--- scraping.js
+++ scraping.js
@@ -1,28 +1,23 @@
-const express = require("express")
 const axios = require("axios");
-const cheerio = require("cheerio")
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const cheerio = require("cheerio");
+const fs = require("fs");
 
 const url = "https://elpais.com/ultimas-noticias/";
 let noticias = [];
 
-app.get("/", async (req, res) => {
+const noticiasDia = async () => {
     try {
         const response = await axios.get(url);
         if (response.status === 200) {
             const html = response.data;
             const $ = cheerio.load(html);
-            const title = $("title").text() 
-            
+
             $('article.c.c-d.c--m').each((index, element) => {
                 const titulo = $(element).find('.c_t').text().trim() || "Sin título";
                 const imagen = $(element).find('.c_m_e').attr('src') || "Sin imagen";
                 const descripcion = $(element).find('.c_d').text().trim() || "Sin descripción";
                 const enlace = $(element).find('a').attr('href') || "#";
 
-                
                 noticias.push({
                     titulo,
                     imagen,
@@ -31,31 +26,15 @@ app.get("/", async (req, res) => {
                 });
             });
 
-            res.send(`
-                <h1>${title}</h1>
-                <a href="/noticias">
-                <p>Accedé a Las Últimas Noticias de El País</p>
-                </a>
-            `);
+            
+            fs.writeFileSync('noticias.json', JSON.stringify(noticias, null, 2));
+
+            return noticias;
         }
     } catch (error) {
-        console.error("Error al procesar la solicitud:", error);
-        res.status(500).send("Error interno del servidor");
+        console.error("Error En La Solicitud:", error.status);
+        
     }
-});
+};
 
-app.get("/noticias", (req, res) => {
-    res.send(noticias)
-})
-
-app.use((req, res) => {
-    res.status(404).send(`
-        <h1>404 - Página no Encontrada</h1>
-        a href="/">Volver al Home</a>
-        `);
-});
-
-app.listen(8000, () => {
-    console.log("Server Linstening On PORT, http://localhost:8000");
-})
-console.log("JSON NOTICIAS", noticias);
+module.exports = noticiasDia;
